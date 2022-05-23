de4937d19304e62d23294b32a6890e4b17960177
Anton Thunberg <anton@thunberg.nu>
2022-05-23 12:53:34 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const cameraButton = document.querySelector('#start-camera');
+const videoElem = document.querySelector('#camera');
+const takePicButton = document.querySelector('#take-picture');
+//canvas
+const galleryElem = document.querySelector('#gallery');
